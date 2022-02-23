b28bcb34d0cce4b1324e7df317d854f368a3e2cc
Furqaan Thakur <thakurfurqaan@gmail.com>
2022-02-23 17:12:48 +0530

    Database

--- Teradexa/src/settings.py
+++ Teradexa/src/settings.py
@@ -83,10 +83,15 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd7dphc4k2oc978',
+        'USER': 'noyrjvakschool',
+        'PASSWORD': '5b853eb74731dbf6d1b840fb0d20145070c8b33bb7b01361faa2b24bd8fea7f8',
+        'HOST': 'ec2-54-156-110-139.compute-1.amazonaws.com',
+        'PORT': '5432',
     },
     'productdb': {
        'ENGINE': 'django.db.backends.postgresql',
